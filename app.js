9a5c7b28ab35eac05785a4ef9b11d1a639c0c1ea
NoamDan645 <noamdan7@gmail.com>
2024-01-31 17:06:09 +0200

    connected to mongo

--- app.js
+++ app.js
@@ -3,25 +3,15 @@ const app = express()
 const dotenv = require('dotenv').config()
 //const port = process.env.PORT || 3000;
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+const db = mongoose.connection
+db.on('error',error=>{console.error(error)})
+db.once('open',()=>console.log('connected to mongo'))
+
+//mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser : True})
 const postRouter = require('./routes/post_route.js')
 app.use('/post',postRouter)
-
-// //קוד שירוץ רק אם מישוה בעתיד יקרא לשרת ויבקש את הרות (השורש)
-// app.get('/' , (req,res)=>{
-//     res.send("hello")
-// })
-
-// app.get('/index1' , (req,res)=>{
-//     res.send("indcdcdex1")
-// })
-
-// app.get('/index2' , (req,res)=>{
-//     res.send("index2 bla ccscscsccs")
-// })
-
-
-//callback function that run in future 
-
 
 app.listen(process.env.PORT, (error) => {
     if (error) {
